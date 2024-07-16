fbfbb0c059dd08caad21fe418fab82311c9bc99f
Pascal Onaho <156159318+Onaho-Pascal@users.noreply.github.com>
2024-07-16 21:57:31 +0100

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -13,3 +13,11 @@ chmod 600 $PASSFILE
 
 # This part of the code checks if the script was called with exactly one argument. If not, it prints a usage message  
 # showing how to run the script correctly and then exits with an error status code. 
+
+mkdir -p /var/secure
+PASSFILE="/var/secure/user_passwords.txt"
+touch $PASSFILE
+chmod 600 $PASSFILE
+
+# This part of the script ensures that the directory 'var/secure' exists, creates a file called user_passwords.txt in that directory  
+# to also set its permission so that only the owner can read and write to it.
