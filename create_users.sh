a525fbd845c16c1df85b8d7fc1547cd30d0c1e4e
Pascal Onaho <156159318+Onaho-Pascal@users.noreply.github.com>
2024-07-16 21:29:16 +0100

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -10,3 +10,6 @@ mkdir -p /var/secure
 PASSFILE="/var/secure/user_passwords.txt"
 touch $PASSFILE
 chmod 600 $PASSFILE
+
+# This part of the code checks if the script was called with exactly one argument. If not, it prints a usage message  
+# showing how to run the script correctly and then exits with an error status code. 
