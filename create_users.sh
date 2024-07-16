9e76b494832d455b62d1d0918815887fdf71f25a
Pascal Onaho <156159318+Onaho-Pascal@users.noreply.github.com>
2024-07-16 21:15:41 +0100

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,1 +1,12 @@
-#Task
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+        echo "Use: $0 <filename>"
+        exit 1
+fi
+FILENAME=$1
+
+mkdir -p /var/secure
+PASSFILE="/var/secure/user_passwords.txt"
+touch $PASSFILE
+chmod 600 $PASSFILE
